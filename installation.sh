f868191e18c4574b3095f17ef84d9003c9ece1de
kanth-sri <srikanthadepu8008@gmail.com>
2024-10-25 21:36:27 +0530

    installations

---
+++ installation.sh
@@ -0,0 +1,32 @@
+
+#!/bin/bash
+echo "Disk resizing..."
+sudo growpart /dev/xvda 4
+sudo lvextend -l +50%FREE /dev/RootVG/rootVol
+sudo lvextend -l +50%FREE /dev/RootVG/varVol
+sudo xfs_growfs /
+sudo xfs_growfs /var
+
+echo "Installing kubectl...."
+sudo curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
+if [ $? -eq 0 ] 
+then
+    chmod +x ./kubectl
+    sudo mv kubectl /usr/local/bin/kubectl
+fi
+
+kube_ver = kubectl -version
+echo "kube_version: $kube_ver"
+
+echo "Installing eksctl....."
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+sudo curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+if [ $? -eq 0 ] 
+then
+    tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+    sudo mv /tmp/eksctl /usr/local/bin
+    echo "SUCCESS...eksctl"
+fi
+echo "please proceed with AWS CONFIGURE"
+
