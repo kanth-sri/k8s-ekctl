e72ede3f36ec5270ba132ef83a93db734f7b9cb4
kanth-sri <srikanthadepu8008@gmail.com>
2024-10-26 15:08:01 +0530

    k8s-eksctl

--- installation.sh
+++ installation.sh
@@ -11,7 +11,7 @@ echo "Installing kubectl...."
 sudo curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/kubectl
-kubectl -version
+kubectl version
 
 
 echo "Installing eksctl....."
