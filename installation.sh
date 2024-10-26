4a5c685ece4458fdc6e2d9e24948ee93c016aee9
kanth-sri <srikanthadepu8008@gmail.com>
2024-10-26 15:07:00 +0530

    k8s-eksctl

--- installation.sh
+++ installation.sh
@@ -9,10 +9,10 @@ sudo xfs_growfs /var
 
 echo "Installing kubectl...."
 sudo curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
-chmod +x ./kubectl
+sudo chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/kubectl
-kube_ver = kubectl -version
-echo "kube_version: $kube_ver"
+kubectl -version
+
 
 echo "Installing eksctl....."
 ARCH=amd64
@@ -20,7 +20,7 @@ PLATFORM=$(uname -s)_$ARCH
 sudo curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 if [ $? -eq 0 ] 
 then
-    tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+    sudo tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
     sudo mv /tmp/eksctl /usr/local/bin
     echo "SUCCESS...eksctl"
 fi
